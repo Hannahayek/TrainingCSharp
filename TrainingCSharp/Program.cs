7ab79714f1c52f5c1c7447be2d707d49fd6cead2
Hannahayek <42571381+Hannahayek@users.noreply.github.com>
2018-09-30 17:00:39 +0800

    20

--- TrainingCSharp/Program.cs
+++ TrainingCSharp/Program.cs
@@ -6,7 +6,31 @@ namespace TrainingCSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("{0} {1}",float.MinValue,float.MaxValue);
+            //Console.WriteLine("{0} {1}",float.MinValue,float.MaxValue);
+
+            //int i = 1;
+            //byte b = (byte)i;
+
+            //var number = "12345";
+            //int i = Convert.ToInt32(number);
+            //Console.WriteLine(i);
+
+
+            try
+            {
+                var number = "12345";
+                byte b = Convert.ToByte(number);
+                Console.WriteLine(b);
+
+
+
+              
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("number not converted");
+            }
             Console.ReadLine();
         }
     }
