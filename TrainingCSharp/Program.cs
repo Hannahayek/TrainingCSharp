60dc31c11aed64164fd03428c0687175c035ad7c
hayek <hayek@HANNA-PC>
2018-10-04 23:03:58 +0800

    arrays 31

--- TrainingCSharp/Program.cs
+++ TrainingCSharp/Program.cs
@@ -1,3 +1,4 @@
+using System;
 using TrainingCSharp.Math;
 
 namespace TrainingCSharp
@@ -17,7 +18,36 @@ namespace TrainingCSharp
             var result = calculater.Add(10, 11);
         }
     }
+
+
+    class ArraysProgram
+    {
+    static void Main(string[] args)
+    {
+
+        var numbers = new int[3];
+        numbers[0] = 1;
+        Console.WriteLine(numbers[0]);
+        Console.WriteLine(numbers[1]);
+        Console.WriteLine(numbers[2]);
+
+        var flags = new bool[3];
+        flags[0] = true;
+        Console.WriteLine(flags[0]);
+        Console.WriteLine(flags[1]);
+        Console.WriteLine(flags[2]);
+
+        var names=new string[3]{"jack","hanna","batal"};
+        Console.WriteLine(names[0]);
+        Console.WriteLine(names[1]);
+        Console.WriteLine(names[2]);
+            Console.Read();
+    }
+
+    }
 }
+
+
 
 
 
