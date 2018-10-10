95467a4f06752c3461c95274237a4e2323374133
Hannahayek <42571381+Hannahayek@users.noreply.github.com>
2018-10-10 17:33:20 +0800

    42. Demo: If/Else

--- TrainingCSharp/Program.cs
+++ TrainingCSharp/Program.cs
@@ -167,6 +167,67 @@ c:\folder1\folder3";
             }
 
         }
+
+
+class DemoIfElse
+        {
+
+            static void Main(string[] args)
+            {
+
+
+                //int hour = 10;
+
+                //if (hour > 0 && hour < 12)
+                //{
+                //    Console.WriteLine("It is Morning");
+                //}
+
+                //else if (hour >= 12 && hour < 18)
+                //{
+                //    Console.WriteLine("it is afternoon");
+                //}
+
+
+                //else
+                //{
+                //    Console.WriteLine("it is evening");
+
+                //}
+
+
+                bool isGoldCustomer = true;
+
+                //float price;
+
+                //if (isGoldCustomer)
+                //    price = 19.5f;
+
+                //else
+                //    price = 29.95f;
+                // above if can be done as below
+
+                float price = (isGoldCustomer) ? 19.5f : 29.95f;
+                Console.WriteLine(price);
+                Console.ReadLine();
+
+
+
+
+
+            }
+
+            }
+
+
+
+
+
+
+
+
+
+
     }
 }
 
