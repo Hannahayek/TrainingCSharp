4dbcba331ca7f08d4c2a7add67202c4699ee65ed
Hannahayek <42571381+Hannahayek@users.noreply.github.com>
2018-10-11 17:36:38 +0800

    47. Demo: While Loops

--- TrainingCSharp/Program.cs
+++ TrainingCSharp/Program.cs
@@ -455,7 +455,65 @@ class DemoIfElse
 
     }
 
+    class WhileLoops
+    {
 
+        static void Main(string[] args)
+        {
+            //for (var i = 1; i <= 10; i++)
+            //{
+
+            //    if (i % 2 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+
+
+            //}
+
+            //var i = 0;
+            //while(i<=10)
+            //{
+            //    if (i % 2 == 0)
+
+            //      Console.WriteLine(i);
+            //    i++;
+
+
+            //}
+
+
+
+
+            //while(true)
+            //{
+            //    Console.Write("Enter your name: ");
+            //    var input = Console.ReadLine();
+            //    if (String.IsNullOrWhiteSpace(input))
+            //        break;
+            //    Console.WriteLine("@Echo: "+input);
+
+            //}
+            //same result as above but what continue
+            while (true)
+            {
+                Console.Write("Enter your name: ");
+                var input = Console.ReadLine();
+                if (!String.IsNullOrWhiteSpace(input))
+                {
+
+                    Console.WriteLine("@Echo: " + input);
+                    continue;
+                }
+
+                break;
+            }
+
+
+        }
+
+
+    }
 
 }
 
