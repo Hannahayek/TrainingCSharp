9c850594fbaa43a99fd07404f5c30ee20c1e7647
hayek <hayek@HANNA-PC>
2018-10-14 23:30:27 +0800

    56. Exercises

--- TrainingCSharp/Program.cs
+++ TrainingCSharp/Program.cs
@@ -1,5 +1,10 @@
 using System;
+using System.CodeDom;
+using System.Collections.Generic;
 using System.Dynamic;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using System.Threading;
 using TrainingCSharp.Math;
 
 namespace TrainingCSharp
@@ -604,7 +609,7 @@ c:\folder1\folder3";
         }
 
 
-       
+
 
 
     }
@@ -644,32 +649,33 @@ c:\folder1\folder3";
 
         static void Main(string[] args)
         {
-         
+
             var random = new Random();
             var number = random.Next(1, 10);
 
 
-                for (int i = 1; i <= 4; i++)
-                {
+            for (int i = 1; i <= 4; i++)
+            {
 
-                    Console.WriteLine("Enter a number, you have 4 chances");
-                    Console.WriteLine("secret is " + number);
-                    var input = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter a number, you have 4 chances");
+                Console.WriteLine("secret is " + number);
+                var input = Convert.ToInt32(Console.ReadLine());
 
                 if (input == number)
-                    {
-                        Console.WriteLine("you won");
-                        return;
-                    }
-
-                    Console.WriteLine("try again");
+                {
+                    Console.WriteLine("you won");
+                    return;
                 }
+
+                Console.WriteLine("try again");
+            }
+
             Console.WriteLine("lost");
             Console.ReadLine();
         }
 
-          
-   }
+
+    }
 
     /// <summary>
     /// Write a program and ask the user to enter a series of numbers separated by comma. Find the maximum of the 
@@ -686,7 +692,7 @@ c:\folder1\folder3";
             Console.WriteLine("enter numbers seperate by ,");
             var input = Console.ReadLine();
             var numbers = input.Split(',');
-            
+
             //lets assume first number is a max
 
             var max = Convert.ToInt32(numbers[0]);
@@ -703,35 +709,35 @@ c:\folder1\folder3";
             }
 
 
-            Console.WriteLine("max number is "+max);
+            Console.WriteLine("max number is " + max);
             Console.ReadLine();
         }
 
-        
+
     }
 
 
 
-    class DemoArrays55
+    class DemoArrays53
     {
 
         static void Main(string[] args)
         {
             var numbers = new[] {1, 8, 4, 9, 6, 7};
-           
+
 
 
             //length
-            Console.WriteLine("length "+numbers.Length);
+            Console.WriteLine("length " + numbers.Length);
 
             //indexof
 
             var number = Array.IndexOf(numbers, 4);
-            Console.WriteLine("index of 4 ="+number);
+            Console.WriteLine("index of 4 =" + number);
 
             //clear
             //0 starting index 2 length of elements
-            Array.Clear(numbers,0,2);
+            Array.Clear(numbers, 0, 2);
             Console.WriteLine("Effects of clear");
             foreach (var str in numbers)
             {
@@ -741,7 +747,7 @@ c:\folder1\folder3";
             //copy
             var another = new int[3];
 
-           Array.Copy(numbers,another,3);
+            Array.Copy(numbers, another, 3);
             Console.WriteLine("Effects of copy");
             foreach (var str in another)
             {
@@ -751,7 +757,7 @@ c:\folder1\folder3";
             //sort method
 
             Array.Sort(numbers);
-          
+
             Console.WriteLine("Effects of sort");
             foreach (var str in numbers)
             {
@@ -775,6 +781,308 @@ c:\folder1\folder3";
             Console.ReadLine();
         }
 
+    }
+
+
+    class DemoLists55
+    {
+
+        static void Main(string[] args)
+        {
+
+            var numbers = new List<int>() {1, 2, 3, 4, 1};
+
+            numbers.Add(1);
+            //add array to the list
+            numbers.AddRange(new int[3] {4, 6, 7});
+
+
+            foreach (var number in numbers)
+                Console.WriteLine(number);
+
+            //    Console.WriteLine();
+            //    Console.WriteLine("index of 1 is" + numbers.IndexOf(1));
+            //Console.WriteLine("last index of 1 is" + numbers.LastIndexOf(1));
+            //    Console.WriteLine("count is" + numbers.Count);
+
+
+            //remove ,removes with number=4
+            //numbers.Remove(4);
+            //here we remove all numbers=4
+            for (var i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] == 1)
+                    numbers.Remove(numbers[i]);
+            }
+
+            //after removing number 1
+            Console.WriteLine("removed number 4");
+            foreach (var number in numbers)
+                Console.WriteLine(number);
+
+            //clear everything
+            numbers.Clear();
+
+
+            Console.ReadLine();
+        }
+
+
+    }
+
+    ///1- When you post a message on Facebook, depending on the number of people who like your post, Facebook displays different information.
+
+    //If no one likes your post, it doesn't display anything.
+    //If only one person likes your post, it displays: [Friend's Name] likes your post.
+    //If two people like your post, it displays: [Friend 1] and[Friend 2] like your post.
+    //If more than two people like your post, it displays: [Friend 1], [Friend 2] and[Number of Other People] others like your post.
+
+    class Exercises551
+    {
+
+        static void Main(string[] args)
+        {
+            var names = new List<String>();
+
+            while (true)
+            {
+                Console.WriteLine("enter Name or hit Enter to quit");
+
+                var input = Console.ReadLine();
+                if (String.IsNullOrWhiteSpace(input))
+                    break;
+
+
+                names.Add(input);
+
+
+
+            }
+
+            if (names.Count == 1)
+                Console.WriteLine("{0} liked your post ", names[0]);
+
+            else if (names.Count == 2)
+                Console.WriteLine("{0},{1} liked your post", names[0], names[1]);
+
+            else if (names.Count > 2)
+                Console.WriteLine("{0},{1}, liked your post and number {2} others liked your post", names[0], names[1],
+                    names.Count - 2);
+            else
+            {
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    /// Ask the user to enter their name. Use an array to reverse the name and then store the result in a new string. 
+    /// Display the reversed name on the console.
+    /// </summary>
+    class Exercises552
+    {
+
+        static void Main(string[] args)
+        {
+
+            Console.Write("What's your name? ");
+            var name = Console.ReadLine();
+            //he will keep shifting the character pleces -1 until loop is done
+            var array = new char[name.Length];
+            for (var i = name.Length; i > 0; i--)
+                array[name.Length - i] = name[i - 1];
+
+            var reversed = new string(array);
+            Console.WriteLine("Reversed name: " + reversed);
+
+
+
+
+
+            Console.ReadLine();
+        }
+
+
+
+
+
+    }
+
+    /// <summary>
+    /// Write a program and ask the user to enter 5 numbers. If a number has been previously entered, display 
+    /// an error message and ask the user to re-try. Once the user successfully enters 5 unique numbers, sort them 
+    /// and display the result on the console.
+    /// </summary>
+
+
+    class Exercises553
+    {
+
+        static void Main(string[] args)
+        {
+
+            Console.Write("Please Enter 5 Numbers ");
+
+            var numbers = new List<int>();
+
+            while (numbers.Count < 5)
+            {
+
+
+                var input = Convert.ToInt32(Console.ReadLine());
+                if (numbers.Contains(input))
+                {
+                    Console.WriteLine("Number {0} exists please enter new Number", input);
+                    continue;
+
+                    //continue forloop will keep going until pass the value condtion
+                }
+
+                numbers.Add(input);
+            }
+
+
+            numbers.Sort();
+            Console.WriteLine("sorting numbers");
+            foreach (var number in numbers)
+            {
+
+                Console.WriteLine(number);
+            }
+
+            Console.ReadLine();
+
+        }
+
+
+    }
+
+
+
+
+
+    /// <summary>
+    /// Write a program and ask the user to continuously enter a number or type "Quit" to exit. The list of numbers may 
+    /// include duplicates. Display the unique numbers that the user has entered.
+    /// </summary>
+
+    class Exercises554
+    {
+
+        static void Main(string[] args)
+        {
+
+            Console.Write("Please Enter numbers or Quit to exit");
+
+            var numbers = new List<int>();
+
+            while (true)
+            {
+
+
+                var input = Console.ReadLine();
+                if (input.ToLower()=="quit")
+                {
+                    break;
+
+                    //continue forloop will keep going until pass the value condtion
+                }
+                
+                numbers.Add(Convert.ToInt32(Console.ReadLine()));
+            }
+
+
+
+            var uniq = new List<int>();
+
+            foreach (var number in numbers)
+            {
+                if(!uniq.Contains(number))
+                    uniq.Add(number);
+            }
+
+            Console.WriteLine("sorted one");
+
+            foreach (var numberq in uniq)
+            {
+                Console.WriteLine(numberq);
+            }
+            Console.ReadLine();
+
+
+        }
+
+    }
+
+
+    /// <summary>
+    /// Write a program and ask the user to supply a list of comma separated numbers (e.g 5, 1, 9, 2, 10). If the list is 
+    /// empty or includes less than 5 numbers, display "Invalid List" and ask the user to re-try; otherwise, display 
+    /// the 3 smallest numbers in the list.
+    /// 
+    /// </summary>
+
+    class Exercises555
+    {
+
+        static void Main(string[] args)
+        {
+
+            Console.Write("Please Enter 5 numbers seprated by Coma");
+
+            string[] elements;
+
+            var input = Console.ReadLine();
+            while (true)
+            {
+
+
+                if (!String.IsNullOrWhiteSpace(input))
+                {
+                    elements = input.Split(',');
+                    if (elements.Length >= 5)
+                        break;
+
+
+
+
+                    Console.WriteLine("invalid list");
+                }
+            }
+
+            var numbers = new List<int>();
+       
+                foreach(var elem in elements)
+            {
+             numbers.Add(Convert.ToInt32(elem));
+            }
+
+                var smallest=new List<int>();
+
+
+            while (smallest.Count < 3)
+            {
+                //assume the smallest is first one
+
+                var min = numbers[0];
+
+                foreach (var number in numbers)
+                {
+                    if (number < min)
+                        min = number;
+                   
+                }
+                smallest.Add(min);
+                numbers.Remove(min);
+
+            }
+
+            Console.WriteLine("The 3 smallest numbers are: ");
+            foreach (var number in smallest)
+                Console.WriteLine(number);
+            Console.ReadLine();
+        }
 
     }
 
@@ -785,7 +1093,43 @@ c:\folder1\folder3";
 
 
 
-} 
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
