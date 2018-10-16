6e933977e80b01921e4814f60dbebf34943dae47
hayek <hayek@HANNA-PC>
2018-10-16 13:28:56 +0800

    59. DateTime

--- TrainingCSharp/Program.cs
+++ TrainingCSharp/Program.cs
@@ -541,7 +541,38 @@ c:\folder1\folder3";
 
 
 
-   
+    class DateTime59
+    {
+
+        static void Main(string[] args)
+        {
+            var dateTime=new DateTime(2015,1,1);
+            var now = DateTime.Now;
+            var today = DateTime.Today;
+
+
+            Console.WriteLine("Hours: "+now.Hour);
+            Console.WriteLine("Minute: " + now.Minute);
+
+            var tomorrow = now.AddDays(1);
+            var yesterday = now.AddDays(-1);
+            Console.WriteLine(now.ToShortDateString());
+            Console.WriteLine(now.ToLongDateString());
+            Console.WriteLine(now.ToShortTimeString());
+            Console.WriteLine(now.ToLongTimeString());
+            Console.WriteLine(now.ToString("yy-MM-dd"));
+
+
+
+
+
+
+            Console.ReadLine();
+            
+        }
+
+
+    }
 
 
 
