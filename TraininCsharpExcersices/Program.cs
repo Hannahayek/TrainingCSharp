6572167c41f447f5f9c6155b318c68d226781129
hayek <hayek@HANNA-PC>
2018-10-16 20:22:08 +0800

    68. Exercises

--- TraininCsharpExcersices/Program.cs
+++ TraininCsharpExcersices/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -476,6 +477,7 @@ namespace TraininCsharpExcersices
             {
                 Console.WriteLine(numberq);
             }
+
             Console.ReadLine();
 
 
@@ -541,6 +543,7 @@ namespace TraininCsharpExcersices
                         min = number;
 
                 }
+
                 smallest.Add(min);
                 numbers.Remove(min);
 
@@ -555,7 +558,211 @@ namespace TraininCsharpExcersices
     }
 
 
+
+    /// <summary>
+    /// Write a program and ask the user to enter a few numbers separated by a hyphen. Work out 
+    /// if the numbers are consecutive. For example, if the input is "5-6-7-8-9" or "20-19-18-17-16", 
+    /// display a message: "Consecutive"; otherwise, display "Not Consecutive".
+
+    //Section 8, Lecture 68
+    class Exercises681
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Numbers sperated by -");
+            var input = Console.ReadLine();
+
+            var numbers = new List<int>();
+
+            foreach (var number in input.Split('-'))
+                numbers.Add(Convert.ToInt32(number));
+
+            numbers.Sort();
+
+
+            var consecutive = true;
+
+            for (int i = 1; i < numbers.Count; i++)
+            {
+                if (numbers[i] != numbers[i - 1] + 1)
+                    consecutive = false;
+                break;
+
+            }
+
+            var massage = consecutive ? "consecutive" : "notconsecutive";
+            Console.WriteLine(massage);
+            Console.ReadLine();
+        }
+
+
+    }
+
+    /// <summary>
+    /// Write a program and ask the user to enter a few numbers separated by a hyphen. If the user simply 
+    /// presses Enter without supplying an input, exit immediately; otherwise, check to see if there are 
+    /// any duplicates. If so, display "Duplicate" on the console.
+    /// </summary>
+    class Exercises682
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter numbers seprated by -");
+            var input = Console.ReadLine();
+
+            if (String.IsNullOrWhiteSpace(input))
+                return;
+
+            var numbers = new List<int>();
+
+
+
+
+            foreach (var number in input.Split('-'))
+                numbers.Add(Convert.ToInt32(number));
+
+
+
+            var uniques = new List<int>();
+            var duplicates = false;
+            foreach (var number in numbers)
+            {
+                if (!uniques.Contains(number))
+                    uniques.Add(number);
+
+
+                else
+                {
+                    duplicates = true;
+                    break;
+                }
+
+
+            }
+
+            if (duplicates)
+                Console.WriteLine("Duplicate");
+            Console.ReadLine();
+        }
+
+
+    }
+
+
+    /// <summary>
+    /// Write a program and ask the user to enter a time value in the 24-hour time format (e.g. 19:00).
+    /// A valid time should be between 00:00 and 23:59. If the time is valid, display "Ok"; otherwise, 
+    /// display "Invalid Time". If the user doesn't provide any values, consider it as invalid time. 
+    /// </summary>
+
+    class Exercises683
+    {
+
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter time: ");
+            var input = Console.ReadLine();
+
+            if (String.IsNullOrWhiteSpace(input))
+            {
+                Console.WriteLine("Invalid Time");
+                return;
+            }
+
+
+            var components = input.Split(':');
+
+           
+            try
+            {
+                var hour = Convert.ToInt32(components[0]);
+                var minute = Convert.ToInt32(components[1]);
+
+                if (hour >= 0 && hour <= 23 && minute >= 0 && minute <= 59)
+                    Console.WriteLine("Ok");
+                else
+                    Console.WriteLine("Invalid Time");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid Time");
+            }
+        }
+
+    }
+
+    /// <summary>
+    /// Write a program and ask the user to enter a few words separated by a space. Use the words to 
+    /// create a variable name with PascalCase convention. For example, if the user types: 
+    /// "number of students", display "NumberOfStudents". Make sure the program is not dependent on 
+    /// the casing of the input. So if the input is "NUMBER OF STUDENTS", it should still display 
+    /// "NumberOfStudents". If the user doesn't supply any words, display "Error".
+    /// </summary>
+
+
+    class Exercises684
+    {
+
+        static void Main(string[] args)
+        {
+            Console.Write("enter a word ");
+            var input = Console.ReadLine();
+
+            
+
+            if (String.IsNullOrWhiteSpace(input))
+            {
+                Console.WriteLine("Error");
+                return;
+            }
+
+            var variable = "";
+
+            foreach (var word in input.Split(' '))
+            {
+                var pascalword = char.ToUpper(word[0]) + word.ToLower().Substring(1);
+                variable += pascalword;
+
+            }
+
+            Console.WriteLine(variable);
+            Console.ReadLine();
+
+        }
+    }
+
+    /// <summary>
+    /// Write a program and ask the user to enter an English word. Count the number of vowels 
+    /// (a, e, o, u, i) in the word. So, if the user enters "inadequate", the program should display 
+    /// 6 on the console. Make sure the program calculates the number of vowels irrespective of the 
+    /// casing of the word (eg "Inadequate", "inadequate" and "INADEQUATE" all include 6 vowels).
+    class Exercises685
+    {
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a word: ");
+            // Note the ToLower() here. This is used to count for both A and a. 
+            var input = Console.ReadLine().ToLower();
+
+            var vowels = new List<char>(new char[] { 'a', 'e', 'o', 'u', 'i' });
+            var vowelsCount = 0;
+            foreach (var character in input)
+            {
+                if (vowels.Contains(character))
+                    vowelsCount++;
+            }
+
+            Console.WriteLine(vowelsCount);
+        }
+    }
+
 }
+
+
 
 
 
