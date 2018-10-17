de52baef9cb56b378dee7b7721f462a24f1bef55
hayek <hayek@HANNA-PC>
2018-10-17 22:14:26 +0800

    80. Removing Side Effects 9:15

---
+++ CSharpFundamentals/CSharpFundamentals/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharpFundamentals
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var numbers = new List<int> { 1, 2 };
+            var smallests = GetSmallests(numbers, 3);
+
+            foreach (var number in smallests)
+                Console.WriteLine(number);
+            Console.ReadLine();
+        }
+
+        public static List<int> GetSmallests(List<int> list, int count)
+        {       var  buffer=new List<int>(list);  //we add new list so not to remove from ogriginal passed one
+            var smallests = new List<int>();
+
+            while (smallests.Count < count)
+            {
+                var min = GetSmallest(buffer);
+                smallests.Add(min);
+                buffer.Remove(min);
+            }
+
+            return smallests;
+        }
+
+        public static int GetSmallest(List<int> list)
+        {
+            // Assume the first number is the smallest
+            var min = list[0];
+            for (var i = 1; i < list.Count; i++)
+            {
+                if (list[i] < min)
+                    min = list[i];
+            }
+            return min;
+        }
+    }
+}
