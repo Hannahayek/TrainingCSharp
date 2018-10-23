44c344ad9bb956d4a0fc5ee2f6776034f86765c9
hayek <hayek@HANNA-PC>
2018-10-23 22:08:34 +0800

    Section 2-11. Fields

---
+++ Fields/Customers.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace Fields
+{
+    public class Customers
+    {
+        public int Id;
+        public string Name;
+        //public List<Order> Orders; can be initialzed as below both works
+        public readonly List<Order> Orders=new List<Order>();
+   
+
+        public Customers(int id)
+        {
+            this.Id = id;
+        }
+
+        public Customers(string name,int id)
+            :this(id) //instead of initiliaze id we call customer(id)
+        {
+            this.Name = name;
+        }
+
+        public void Promote()
+        {
+            ////..../with read only  below will not be compiled
+            //Orders=new List<Order>();
+        }
+    }
+}
---
+++ Fields/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fields
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer=new Customers(1);
+            customer.Orders.Add(new Order());
+            customer.Orders.Add(new Order());
+
+            //if we call the method Promote because it will initialise orders again all data will be lost
+            //customers.Orders.count will be 0
+            Console.WriteLine(customer.Orders.Count);
+            Console.ReadLine();
+
+        }
+    }
+}
