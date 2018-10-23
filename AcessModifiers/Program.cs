2410fc66275f6b96eae144b552e2d9acdcd9b2d4
hayek <hayek@HANNA-PC>
2018-10-23 22:30:24 +0800

    Section 2-12. Access Modifiers

---
+++ AcessModifiers/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcessModifiers
+{
+
+    public class Person
+    {// with private should start with _
+        //methords should be Camelcase 
+        private DateTime _birthdate;
+
+        public void SetBirthdate(DateTime birthdate)
+        {
+            _birthdate = birthdate;
+        }
+        
+        public DateTime GetBirthdate()
+        {
+            return _birthdate;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //to access private birth date we use methods
+            var person=new Person();
+           person.SetBirthdate(new DateTime(1982,1,1));
+            Console.WriteLine(person.GetBirthdate());
+            Console.ReadLine();
+
+
+        }
+    }
+}
