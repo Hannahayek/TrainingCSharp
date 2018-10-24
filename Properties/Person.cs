39c0c982b8c1c4d13e51e6bf1b8b88285c98608c
Hannahayek <42571381+Hannahayek@users.noreply.github.com>
2018-10-24 17:36:55 +0800

    13. Properties

---
+++ Properties/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties
+{
+    public class Person
+    {
+
+        public string Name { get; set; }
+        //we define constructor because set is private in Birthdate 
+        public Person(DateTime birthdate)
+        {
+            Birthdate = birthdate;
+        }
+        public DateTime Birthdate { get; private set; }
+        public int Age
+        {//this cant be like birthday auto implement,because it depends on birthdate
+            get
+            {
+                var timespan = DateTime.Today - Birthdate;
+                var years = timespan.Days / 365;
+                return years;
+            }
+        }
+    }
+}
---
+++ Properties/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties
+{
+  
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var person = new Person(new DateTime(1984, 12, 6));
+            //this will be not compiled when add private set age
+            //person.Birthdate=new DateTime(1984, 12, 6);
+            Console.WriteLine(person.Age);
+            Console.ReadLine();
+        }
+    }
+}
