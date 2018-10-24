faee59a994eb6eaf9e678c823b4a750174bccb3a
hayek <hayek@HANNA-PC>
2018-10-24 21:45:09 +0800

    14. Indexers

---
+++ Indexers/HttpCookie.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+
+namespace Indexers
+{
+    public class HttpCookie
+    {
+        private readonly Dictionary<string, string> _dictionary;
+
+        public HttpCookie()
+        {
+            _dictionary=new Dictionary<string, string>();
+        }
+
+        //we declare an indexer
+        public string this[string key]
+        {
+            get { return _dictionary[key];}
+            set { _dictionary[key] = value; }
+        }
+
+    }
+}
