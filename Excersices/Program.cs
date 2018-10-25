e5ed202cb76f791ddb397c917fd04afee3684f80
Hannahayek <42571381+Hannahayek@users.noreply.github.com>
2018-10-25 13:12:36 +0800

    Section 2 -Excercise 1

---
+++ Excersices/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Excersices
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopwatch = new StopWatch();
+
+            stopwatch.Start();
+            Thread.Sleep(2000);
+
+            stopwatch.Stop();
+
+            stopwatch.Start();
+            Thread.Sleep(3000);
+
+            stopwatch.Stop();
+
+
+            stopwatch.Start();
+            Thread.Sleep(5000);
+
+            stopwatch.Stop();
+            Console.ReadLine();
+        }
+        }
+    }
+
+
---
+++ Excersices/StopWatch.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excersices
+{
+    public class StopWatch
+    {
+        private DateTime _startTime;
+        private DateTime _endTime;
+        private bool _isRunning = false;
+
+       public void Start()
+        {
+
+            if (!_isRunning)
+            {
+                _startTime = DateTime.Now;
+                Console.WriteLine("time started {0}", _startTime);
+                _isRunning = true;
+            }
+            else
+                throw new InvalidOperationException("started twice");
+
+        }
+
+
+
+        public void Stop()
+        {
+
+            if (_isRunning)
+            {
+                _endTime = DateTime.Now;
+                Console.WriteLine("time started {0}", _endTime);
+                _isRunning = false;
+                GetDuration();
+            }
+        }
+
+        private void GetDuration()
+        {
+            if (_isRunning)
+                throw new InvalidOperationException("Cannot get duration as stopwatch hasn't been started/stopped properly.");
+            var timedif = _endTime - _startTime;
+            Console.WriteLine("Time passed is: {0}", timedif.ToString());
+          
+        }
+
+    }
+}
