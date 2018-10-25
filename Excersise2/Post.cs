27b1e1449a33f6be053cd28d6dbeabbb077c42ba
Hannahayek <42571381+Hannahayek@users.noreply.github.com>
2018-10-25 17:30:22 +0800

    Sesction Excersice 2

---
+++ Excersise2/Post.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excersise2
+{
+    public class Post
+    {
+
+    
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public DateTime Created { get; set; }
+    public int Votes { get; private set; }
+
+    public Post()
+    {
+        Votes = 0;
+    }
+
+    public void UpVote()
+    {
+        Votes += 1;
+    }
+
+    public void DownVote()
+    {
+        Votes -= 1;
+    }
+}
+}
---
+++ Excersise2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excersise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var myPost = new Post();
+
+            Console.WriteLine(myPost.Votes);
+
+            myPost.UpVote();
+            myPost.UpVote();
+            myPost.UpVote();
+            myPost.UpVote();
+
+            Console.WriteLine(myPost.Votes);
+
+            myPost.DownVote();
+            myPost.DownVote();
+            myPost.DownVote();
+            Console.WriteLine(myPost.Votes);
+            Console.ReadLine();
+        }
+    }
+}
