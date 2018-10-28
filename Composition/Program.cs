a0f41a49efa6678dba0290606b08fa43d5280a4f
hayek <hayek@HANNA-PC>
2018-10-28 21:00:07 +0800

    19. Composition

---
+++ Composition/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Composition
+{
+
+
+
+class Program
+    {
+        
+       
+        static void Main(string[] args)
+        {
+
+            var dbMigrator=new DbMigrator(new Logger());
+            //or we pass as below way
+
+            var logger=new Logger();
+
+            var installer=new Installer(logger);
+            dbMigrator.Migrate();
+            installer.install();
+            Console.ReadLine();
+        }
+    }
+
+}
