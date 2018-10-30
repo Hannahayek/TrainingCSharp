eb3294173232138179572818cbd7ea796e67ba17
hayek <hayek@HANNA-PC>
2018-10-30 22:50:51 +0800

    22. Access Modifiers

---
+++ Access Modifiers/Program.cs
@@ -0,0 +1,22 @@
+﻿using Amazon;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Access_Modifiers
+{
+
+  
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var customer = new Customer();
+           // Amazon.RateCalculator calculator=new RateCalculator(); //cannot be accessed outside the assembly it is internal
+        }
+    }
+}
---
+++ Amazon/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Amazon
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public void Promote()
+        {
+          var caclulator=new RateCalculator();
+            var rating = caclulator.Calculate(this);
+            Console.WriteLine("changed");
+        }
+        ////private can be called inside class itself/if changed to protected , the Gold Customer class can access it
+        //private int CalculateRating(bool execludeOrders)
+        //{
+        //    return 0;
+        //}
+
+    }
+}
