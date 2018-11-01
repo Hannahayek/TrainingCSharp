dc0ed7601b4e0bc33d14fea907ac20d1c3b98f9f
Hannahayek <42571381+Hannahayek@users.noreply.github.com>
2018-11-01 17:47:03 +0800

    25. Boxing and Unboxing

---
+++ Boxing and Unboxing/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boxing_and_Unboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var list = new ArrayList();
+
+            list.Add(1); //boxing here will happen, because it is int number, not object,arraylist takes object
+            list.Add("Hanna");
+            list.Add(DateTime.Today);
+
+
+            //to avoid boxing we use generic list
+
+            var anotherlist = new List<int>(); ///use this for a way unboxing
+            anotherlist.Add(1);
+
+           
+
+        }
+    }
+}
