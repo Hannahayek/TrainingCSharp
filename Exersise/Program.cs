3b0aa9712720d4a40d9caf8767502d18b6bf971e
hayek <hayek@HANNA-PC>
2018-11-02 14:48:50 +0800

    27. Exercises

---
+++ Exersise/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exersise_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack =new StackList();
+
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+
+            stack.Push("Hayek");
+            stack.Push("Munir");
+            stack.Push("Hanna");
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Exersise/StackList.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Runtime.InteropServices;
+
+namespace Exersise_4
+{
+    public class StackList
+    {
+        readonly ArrayList _arrayList=new ArrayList();
+        public void Push(object obj)
+        {
+
+            if (obj is null)
+            {
+                throw  new InvalidOperationException("object is null");
+            }
+            else
+            {
+                
+                _arrayList.Add(obj);
+            }
+           
+
+
+ 
+        }
+
+       
+
+        public object Pop()
+        {
+
+        
+            if (_arrayList.Count == 0)
+            {
+
+                throw new InvalidOperationException("list is empty");
+            }
+            else
+            {
+                int arrayCount = _arrayList.Count;
+                object arraytoRemove = _arrayList[arrayCount - 1];
+                _arrayList.RemoveAt(arrayCount-1);
+                return arraytoRemove;
+            }
+
+           
+        }
+    }
+}
