773ee58113a82edd538b6312ef5922a49dea7e24
hayek <hayek@HANNA-PC>
2018-11-06 22:20:32 +0800

    29. Abstract Classes and Members/before adding abstract

---
+++ AbstractClasses/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClasses
+{
+    public class Rectangle : Shape
+    {
+
+
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var circle=new Circle();
+            circle.Draw();
+
+
+            var rectangle=new Rectangle();
+
+            rectangle.Draw();
+
+            //before the abstract, because we didnt override the draw method in rectangle , it will not compile 
+        }
+    }
+}
---
+++ AbstractClasses/Shape.cs
@@ -0,0 +1,14 @@
+﻿namespace AbstractClasses
+{
+    public class Shape
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public virtual void Draw()
+        {
+
+        }
+
+    }
+}
