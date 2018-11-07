ea1117447604c0d6c660d3a18495cdb8fd459128
hayek <hayek@HANNA-PC>
2018-11-07 21:05:56 +0800

    Exercise 1: Design a database connection

---
+++ Section5_Excersise1/OracleConnection.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section5_Excersise1
+{
+   public class OracleConnection :DbConnection
+    {
+        public OracleConnection(string conn) : base(conn)
+        {
+        }
+
+        public override void Openconnection()
+        {
+            if(_timeout>TimeSpan.FromSeconds(40))
+               throw new TimeoutException("oracle connection timeout");
+
+            else
+            Console.WriteLine("oracle connection opened");
+        }
+
+        public override void Closeconnection()
+        {
+            Console.WriteLine("oracle connection closed");
+        }
+    }
+}
---
+++ Section5_Excersise1/SqlConnection.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section5_Excersise1
+{
+    public class SqlConnection: DbConnection    
+    {
+        public SqlConnection(string conn) : base(conn)
+        {
+        }
+
+        public override void Openconnection()
+        {
+            if(_timeout>TimeSpan.FromSeconds(40))
+                throw new TimeoutException("oracle connection timeout");
+            else
+               Console.WriteLine("Sql connection opened");
+        
+        }
+
+        public override void Closeconnection()
+        {
+            Console.WriteLine("Sql connection closed");
+        }
+    }
+}
