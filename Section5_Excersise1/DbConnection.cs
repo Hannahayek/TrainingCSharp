7034c1c4146a98142575676b036e311e5cf1db9b
Hannahayek <42571381+Hannahayek@users.noreply.github.com>
2018-11-07 18:45:35 +0800

    fg

---
+++ Section5_Excersise1/DbConnection.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section5_Excersise1
+{
+   public abstract class DbConnection
+    {
+        public String _connection { get; set; }
+        public readonly TimeSpan _timeout = TimeSpan.FromSeconds(40);
+
+        public DbConnection(String conn)
+        {
+            this._connection = conn;
+            if (String.IsNullOrWhiteSpace(conn))
+                throw new ArgumentNullException("connections is null");
+        }
+
+        public abstract void Openconnection();
+        public abstract void Closeconnection();
+
+
+
+    }
+}
