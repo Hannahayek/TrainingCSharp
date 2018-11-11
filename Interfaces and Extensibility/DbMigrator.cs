834eaf1b4bbcbc36e146db121bb68bda6fd7518f
Hannahayek <42571381+Hannahayek@users.noreply.github.com>
2018-11-11 17:18:29 +0800

    35. Interfaces and Extensibility befoer interface

---
+++ Interfaces and Extensibility/DbMigrator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces_and_Extensibility
+{
+   public class DbMigrator
+    {
+        private readonly ILogger _logger;
+        public DbMigrator(ILogger logger)
+        {
+            _logger = logger;
+        }
+        public void Migrate()
+        {
+            Console.WriteLine("migrations started at {0}",DateTime.Now);
+            //detauls of migrating the database
+        }
+
+    }
+}
