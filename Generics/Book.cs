54305461e58ca40cba78ee43efcc22557f164059
hayek <hayek@HANNA-PC>
2018-11-20 22:10:18 +0800

    Generics -1

---
+++ Generics/Book.cs
@@ -0,0 +1,13 @@
+﻿namespace Generics
+{
+    public class Book
+    {
+        public string Isbn { get; set; }
+        public string Title { get; set; }
+        public Book(string isbn,string title)
+        {
+            this.Isbn = isbn;
+            this.Title = title;
+        }
+    }
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var book = new Book ("1111", "Csharp advances");
+            //var numbers=new List();
+            //numbers.Add(10);
+
+            //var books=new BookList();
+            //books.Add(book);
+            var numbers=new GenericList<int>();
+            numbers.Add(10);
+            //here instead of we keep creating objects, we can pass any type of objects
+            var books=new GenericList<Book>();
+            books.Add(new Book("1111", "Csharp advances"));
+
+
+        }
+    }
+}
