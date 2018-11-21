6184292b2b50899bdb8ae7267e71b9234fb8f50d
hayek <hayek@HANNA-PC>
2018-11-21 22:10:16 +0800

    Delgates starter code

---
+++ Delegates/PhotoFilters.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Delegates
+{
+    public class PhotoFilters
+    {
+        public void ApplyBrightness(Photo photo)
+        {
+            Console.WriteLine("apply brightness");
+        }
+
+        public void ApplyContrast(Photo photo)
+        {
+            Console.WriteLine("apply contrast");
+        }
+
+        public void Resize(Photo photo)
+        {
+            Console.WriteLine("resize photo");
+        }
+    }
+}
---
+++ Delegates/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+
+    public class PhotoProcessor
+    {
+        public void Process(string path)
+        {
+            var photo = Photo.load(path);
+            var filters=new PhotoFilters();
+            filters.ApplyBrightness(photo);
+            filters.ApplyContrast(photo);
+            filters.Resize(photo);
+            photo.Save();
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
